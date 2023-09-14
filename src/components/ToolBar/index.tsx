30243bb39508b0de68fce5aa271bc238f06f13b5
DmytroSerdiukov <dmytro.serdiukov@gmail.com>
2023-09-14 14:10:50 +0300

    refactor: Toolbar index.tsx styles, added items

--- src/components/ToolBar/index.tsx
+++ src/components/ToolBar/index.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { FC } from "react";
 import List from "@mui/material/List";
 import { listItems } from "./listItems";
 
-const ToolBar = () => {
+const ToolBar: FC = (): JSX.Element => {
   return (
-    <>
-      <List component="nav">{listItems}</List>
-    </>
+    <List
+      component="nav"
+      sx={{
+        paddingLeft: 1,
+        paddingRight: 1,
+        marginTop: 3,
+        width: 270,
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {listItems}
+    </List>
   );
 };
 
