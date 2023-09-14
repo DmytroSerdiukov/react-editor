a23bca399a27049411668280ef801c19621287dd
DmytroSerdiukov <dmytro.serdiukov@gmail.com>
2023-09-14 16:54:57 +0300

    feature: Preview component

---
+++ src/components/Preview/index.tsx
@@ -0,0 +1,16 @@
+import Container from "@mui/material/Container";
+
+const Preview = () => {
+  return (
+    <Container
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        background: "red",
+      }}
+    ></Container>
+  );
+};
+
+export default Preview;
