3d0d259d57116958493482c9a0304083a4cca370
DmytroSerdiukov <dmytro.serdiukov@gmail.com>
2023-09-14 14:09:28 +0300

    feature: icons and styles for toolbar items

---
+++ src/components/ToolBar/Icons.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ViewHeadlineIcon from "@mui/icons-material/ViewHeadline";
+import ImageIcon from "@mui/icons-material/Image";
+import ArticleIcon from "@mui/icons-material/Article";
+
+export const ICONS: any = {
+  Headline: <ViewHeadlineIcon sx={{ fontSize: 25, color: "#97AACD" }} />,
+  Paragraph: <ArticleIcon sx={{ fontSize: 25, color: "#97AACD" }} />,
+  Button: <ImageIcon sx={{ fontSize: 25, color: "#97AACD" }} />,
+  Image: <ImageIcon sx={{ fontSize: 25, color: "#97AACD" }} />,
+};
--- src/components/ToolBar/Item.tsx
+++ src/components/ToolBar/Item.tsx
@@ -2,16 +2,35 @@ import React, { FC } from "react";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-
-type IItemProps = {
-  title: string;
-};
+import { ICONS } from "./Icons";
+import { IItemProps } from "./types";
 
 const Item: FC<IItemProps> = ({ title }): JSX.Element => {
   return (
-    <ListItemButton>
-      <ListItemIcon></ListItemIcon>
-      <ListItemText primary={title} />
+    <ListItemButton
+      sx={{
+        width: 100,
+        height: 80,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#F6F9FE",
+        margin: 1,
+        paddingTop: 2,
+        borderRadius: 1,
+      }}
+    >
+      <ListItemIcon
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        {ICONS[title]}
+      </ListItemIcon>
+      <ListItemText
+        primary={title}
+        primaryTypographyProps={{ fontSize: "12px", lineHeight: "18px" }}
+        sx={{ color: "#252A32" }}
+      />
     </ListItemButton>
   );
 };
