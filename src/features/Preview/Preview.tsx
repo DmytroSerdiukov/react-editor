1e4cb4e8bdb3170050c067f97786ab8a97b49875
DmytroSerdiukov <dmytro.serdiukov@gmail.com>
2023-09-15 11:02:16 +0300

    refactor: Preview name file

--- src/features/Preview/Preview.tsx
+++ src/features/Preview/Preview.tsx
@@ -2,7 +2,7 @@ import Box from "@mui/material/Box";
 import { styles } from "./styles";
 
 const Preview = () => {
-  return <Box style={styles.wrapper}>dsds</Box>;
+  return <Box sx={styles.wrapper}>dsds</Box>;
 };
 
 export default Preview;
