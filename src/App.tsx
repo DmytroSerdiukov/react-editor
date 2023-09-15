8aeb618cf08f3265687b3b0287a69b3e04e774a8
DmytroSerdiukov <dmytro.serdiukov@gmail.com>
2023-09-15 09:11:25 +0300

    refactor: App.tsx styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,12 @@ const defaultTheme = createTheme();
 const App: FC = (): JSX.Element => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container maxWidth={false}>
+      <Container
+        maxWidth={false}
+        sx={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
         <Header title={"REACT EDITOR Test"} />
-        <main style={{ display: "flex" }}>
+        <main style={{ display: "flex", height: "100%" }}>
           <ToolBar />
           <EditZone />
           <Preview />
