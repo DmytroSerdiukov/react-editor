3e97e89393064b6d25b412ce1523464cc08faaa8
DmytroSerdiukov <dmytro.serdiukov@gmail.com>
2023-09-15 15:27:30 +0300

    feature: EditItem handling

--- src/store/features/EditZoneReducer.ts
+++ src/store/features/EditZoneReducer.ts
@@ -19,7 +19,22 @@ export const EditZoneReducer = createSlice({
   initialState,
   reducers: {
     createNewItem: (state, action) => {
-      state.items.push(action.payload);
+      state.items.push({
+        id: state.items.length,
+        title: action.payload,
+        value: "",
+      });
+    },
+    cloneItem: (state, action) => {
+      state.items.push({ ...action.payload, id: state.items.length });
+    },
+    changeItemValue: (state, action) => {
+      const { id, value } = action.payload;
+      state.items[id].value = value;
+    },
+
+    deleteEditItem: (state, action) => {
+      state.items = state.items.filter((el: any) => el.id !== action.payload);
     },
     getItems: (state: EditZoneState) => {
       return state.items;
@@ -27,6 +42,12 @@ export const EditZoneReducer = createSlice({
   },
 });
 
-export const { getItems } = EditZoneReducer.actions;
+export const {
+  getItems,
+  createNewItem,
+  changeItemValue,
+  deleteEditItem,
+  cloneItem,
+} = EditZoneReducer.actions;
 
 export default EditZoneReducer.reducer;
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ToolBarReducer from "./features/ToolBarReducer";
+import EditZoneReducer from "./features/EditZoneReducer";
 
 export const store = configureStore({
-  reducer: { toolbar: ToolBarReducer },
+  reducer: {
+    toolbar: ToolBarReducer,
+    editzone: EditZoneReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
