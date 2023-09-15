1f7d20fcfd9a5f275413d6a178ffe4e0e4c69ab8
DmytroSerdiukov <dmytro.serdiukov@gmail.com>
2023-09-15 15:20:02 +0300

    refactor: EditZone props type

--- src/components/EditZone/types.ts
+++ src/components/EditZone/types.ts
@@ -1,3 +1,8 @@
 export type IItemProps = {
+  id: number;
   title: string;
+  value: string;
+  changeItemVal: (data: any) => {};
+  deleteItem: (id: any) => {};
+  cloneItem: (data: any) => {};
 };
